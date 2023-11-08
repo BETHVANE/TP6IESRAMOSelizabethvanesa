0887b0bb55a1a35f3f7fca64813a6ba80c2303d3
tiago <tiago@juanamanso>
2023-11-08 15:25:35 -0300

    RAMOS VANESA Inicial TP6- Nuevo proyecto cambios

---
+++ src/main/java/ar/edu/ies6/Tp6Ies6RamoSelizabethvanesaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp6Ies6RamoSelizabethvanesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp6Ies6RamoSelizabethvanesaApplication.class, args);
+	}
+
+}
