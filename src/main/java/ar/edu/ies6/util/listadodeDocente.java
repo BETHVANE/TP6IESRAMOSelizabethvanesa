55e43a14de6a2ffbc9aaa655a97225c028606ec9
tiago <tiago@juanamanso>
2023-11-19 04:48:49 -0300

    ramos vanesa cambios commit

--- src/main/java/ar/edu/ies6/util/listadodeDocente.java
+++ src/main/java/ar/edu/ies6/util/listadodeDocente.java
@@ -3,7 +3,9 @@ package ar.edu.ies6.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import ar.edu.ies6.controller.Docente;
+import ar.edu.ies6.model.Docente;
+
+
 // import ar.edu.ies6.model.Docente;
 public class listadodeDocente {
 
